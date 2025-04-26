74274c2a94bccc8de71290b36a57047e6b04c9d1
SSM-F <melaniebc57@gmail.com>
2025-04-26 13:07:22 +0100

    adding test_upload_file reads loggings

--- test/test_upload_file.py
+++ test/test_upload_file.py
@@ -1,24 +1,18 @@
 from src.upload_file import upload_file
-from src.credentials import get_service_token_credential
-from src.create_directory import create_directory
 
-from azure.storage.filedatalake import DataLakeDirectoryClient
-from unittest.mock import MagicMock,patch
-from mocks import *
-import json
+from azure.storage.filedatalake import DataLakeServiceClient
+
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='example.env') 
 import os
+import logging
 
 
 
 def test_upload_file():
-
     file_system_name = 'myfirstcontainer'
     file_path = 'data/customer_industries.json'
-    #account_name = os.getenv('ACCOUNTNAME')
-    #account_key= os.getenv('ACCOUNTKEY')
     connection_string = os.getenv("CONNSTRING")
    
     
@@ -26,6 +20,29 @@ def test_upload_file():
                            file_system_name,
                            file_path,
                            )
-    assert response == "File uploaded succesfully"
+    assert isinstance(response,DataLakeServiceClient)
+
+def test_upload_file_logs_succesful_message(caplog):
+    file_system_name = 'myfirstcontainer'
+    file_path = 'data/customer_industries.json'
+    connection_string = os.getenv("CONNSTRING")
+
+    with caplog.at_level(logging.INFO):
+        upload_file(connection_string,
+                           file_system_name,
+                           file_path,
+                           )
+    assert "File uploaeded succesfully" in caplog.text
+
+
+
+
+
+    #file_path_2 = 'data/vehicle_customers.json'
+    #response_2 = upload_file(connection_string,
+    #                       file_system_name,
+    #                       file_path_2,
+    #                       )
+
 
 
