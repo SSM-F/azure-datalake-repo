a8f821d592d22cee5346f359b4bd62368b529a71
SSM-F <melaniebc57@gmail.com>
2025-04-26 09:56:24 +0100

    adding test for upload_file

---
+++ test/test_upload_file.py
@@ -0,0 +1,32 @@
+from src.upload_file import upload_file
+from src.credentials import get_service_token_credential
+from src.create_directory import create_directory
+
+from azure.storage.filedatalake import DataLakeDirectoryClient
+from unittest.mock import MagicMock,patch
+from mocks import *
+import json
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='example.env') 
+import os
+
+
+
+def test_upload_file():
+
+    file_system_name = 'myfirstcontainer'
+    file_path = 'data/customer_industries.json'
+    account_name = os.getenv('ACCOUNTNAME')
+    account_key= os.getenv('ACCOUNTKEY')
+    connection_string = os.getenv("CONNSTRING")
+   
+    
+    response = upload_file(connection_string,
+                           file_system_name,
+                           file_path,
+                           account_name,
+                           account_key)
+    assert response == "File uploaded succesfully"
+
+
