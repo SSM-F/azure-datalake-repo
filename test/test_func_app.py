8f4ef06c859404606f81e9160efd7f3967509ec7
SSM-F <melaniebc57@gmail.com>
2025-05-01 22:31:03 +0100

    adding test for request with no name

--- test/test_func_app.py
+++ test/test_func_app.py
@@ -12,3 +12,13 @@ def test_succesfull_request():
     assert response.status_code == 200
     assert response.get_body().decode() == 'Hello there, Alice!'
 
+def test_no_name_request():
+    test_req= func.HttpRequest(
+        method="GET",
+        url="api/azure-data-lake-gen2",
+        params={'name':None},
+        body=None
+    )
+    response = main(test_req)
+    assert response.status_code == 400
+    assert response.get_body().decode() == "Please pass a name, dont be rude"
