e9b2f87e1a3ad3d3afdfe024214f076436032346
SSM-F <melaniebc57@gmail.com>
2025-05-02 19:06:56 +0100

    test for succesful and unsuccesful POST

--- test/test_func_app.py
+++ test/test_func_app.py
@@ -1,24 +1,56 @@
 import azure.functions as func
-from src.func_app import main
+from src.func_app import main,post
+import json
 
-def test_succesfull_request():
-    test_req = func.HttpRequest(
-        method='GET',
-        url='/api/azure-data-lake-gen2',
-        params={'name':'Alice'},
-        body=None
-    )
-    response = main(test_req)
-    assert response.status_code == 200
-    assert response.get_body().decode() == 'Hello there, Alice!'
+class TestGET:
+    def test_succesfull_request(self):
+        test_req = func.HttpRequest(
+            method='GET',
+            url='/api/azure-data-lake-gen2',
+            params={'name':'Alice'},
+            body=None
+        )
+        response = main(test_req)
+        assert response.status_code == 200
+        assert response.get_body().decode() == 'Hello there, Alice!'
 
-def test_no_name_request():
-    test_req= func.HttpRequest(
-        method="GET",
-        url="api/azure-data-lake-gen2",
-        params={'name':None},
-        body=None
-    )
-    response = main(test_req)
-    assert response.status_code == 400
-    assert response.get_body().decode() == "Please pass a name, dont be rude"
+    def test_no_name_request(self):
+        test_req= func.HttpRequest(
+            method="GET",
+            url="/api/azure-data-lake-gen2",
+            params={'name':None},
+            body=None
+        )
+        response = main(test_req)
+        assert response.status_code == 400
+        assert response.get_body().decode() == "Please pass a name, dont be rude"
+
+class TestPOST:
+    def test_succesfull_post(self):
+        test_post_body={'name':'Mario',
+                        'surname':'SuperStar'}
+        request= func.HttpRequest(
+            method='POST',
+            url='/api/azure-data-lake-gen2',
+            body=json.dumps(test_post_body).encode('utf-8'),
+            headers={
+                'Content-Type':'application/json'
+            }
+        )
+        response= post(request)
+        assert response.status_code == 200
+        assert response.get_body().decode() == 'Welcome Mario SuperStar'
+
+    def test_exception_post(self):
+        test_post_body= 'nope'
+        request = func.HttpRequest(
+            method='POST',
+            url='/api/azure-data-lake-gen2',
+            body=test_post_body.encode('utf-8'),
+            headers={
+                'Content-Type':'application/json'
+            }
+        )
+        response = post(request)
+        
+        assert response.status_code== 400
